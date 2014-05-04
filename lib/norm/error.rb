251280dd788da87873e8b5fb3ef1319f592430bb
Ernie Miller <ernie@erniemiller.org>
2014-05-04 10:35:45 -0400

    Remove MemoryRepository errors

--- lib/norm/error.rb
+++ lib/norm/error.rb
@@ -1,9 +1,5 @@
 module Norm
 
   Error = Class.new(StandardError)
-  # TODO: These errors are very memory repository specific, and they suck
-  NotFoundError = Class.new(Error)
-  InvalidKeyError = Class.new(Error)
-  DuplicateKeyError = Class.new(Error)
 
 end
